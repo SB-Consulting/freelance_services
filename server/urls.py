83ec4e26b40178cd0773419df215fc08e3be2474
SaPer663 <saper663ru@gmail.com>
2021-09-12 13:26:58 +0500

    added namespace helpdesk to path '/helpdesk'

--- server/urls.py
+++ server/urls.py
@@ -23,7 +23,7 @@ admin.autodiscover()
 urlpatterns = [
     # Apps:
     # Helpdesk:
-    path('helpdesk/', include(helpdesk_urls)),
+    path('helpdesk/', include(helpdesk_urls, namespace='helpdesk')),
 
     # Health checks:
     path('health/', include(health_urls)),  # noqa: DJ05
